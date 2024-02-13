c15ce0a14a504a1f830af439abc646b37c58704e
Levast David <dasyx@MBP-de-Levast.lan>
2024-02-14 00:18:38 +0100

    add moyenne.py

--- moyenne.py
+++ moyenne.py
@@ -1,10 +1,27 @@
-
-
 def check_number():
     numbers = []
-    ask_number = input("Veuillez saisir un nouveau nombre (pour terminer la saisie, tapez 'fin') : ")
-    while ask_number != "fin":
-        ask_number = input("Veuillez saisir un nouveau nombre (pour terminer la saisie, tapez 'fin') : ")
+    total_sum = 0
+    while True:
+        ask_number = input("Veuillez saisir un nouveau nombre entier (pour terminer la saisie, tapez 'fin') : ")
         if ask_number == "fin":
+            # boucle sur les résultats de la liste pour afficher une moyenne
+            for number in numbers:
+                total_sum += number
+                average = float(total_sum / len(numbers))
+            print(f"La moyenne des nombres saisis est de : {average}")
             break
+        try:
+            num = int(ask_number)
+            numbers.append(num)
+            print(numbers)
+        except:
+            # Si une ValueError est levée, informez l'utilisateur et continuez la boucle
+            print("Ce n'est pas un nombre entier valide. Veuillez réessayer.")
+            continue  # Passez à la prochaine itération de la boucle
+
+        # Calcul de la moyenne en dehors de la boucle while
+        """ if numbers:  # Vérifier que la liste n'est pas vide
+            total_sum = sum(numbers)  # Utilisez la fonction sum() pour la somme
+            average = total_sum / len(numbers)
+            print(f"La moyenne des nombres saisis est de : {average}") """
 check_number()
