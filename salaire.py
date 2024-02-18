d93ce10c5c7a3ac769d30a36910f3b6f9fe3d264
dasyx <seanpnn00@gmail.com>
2024-02-18 16:48:27 +0100

    add separated functions + try & except in salaire.py

--- salaire.py
+++ salaire.py
@@ -1,13 +1,30 @@
+def lire_salaire_mensuel():
+    while True:
+        try:
+            salaire_mensuel = float(input("Saisissez votre salaire mensuel (en euros) : "))
+            if salaire_mensuel < 0:
+                print("Votre salaire doit être un nombre positif.")
+            else:
+                return salaire_mensuel
+        except ValueError:
+            print("Veuillez saisir un nombre valide.")
+
+def lire_heures_semaine():
+    while True:
+        try:
+            heures_semaine = float(input("Saisissez vos heures travaillées par semaine : "))
+            if heures_semaine < 0:
+                print("Votre nombre d'heures travaillées doit être un nombre positif.")
+            else:
+                return heures_semaine
+        except ValueError:
+            print("Veuillez saisir un nombre valide.")
+
 def lire_entrees():
-    while True :
-        salaire_mensuel = float(input(f"Saisissez votre salaire mensuel : "))
-        if salaire_mensuel < 0 :
-            print("Votre salaire doit être un nombre positif")
-            continue
-        else :
-            print(f"Le salaire saisi est de {salaire_mensuel} euros.")
-            return salaire_mensuel
-        
-    """  heures_semaine = float(input(f'Saisissez vos heures travaillées par semaine : {heures} ')) """
+    salaire_mensuel = lire_salaire_mensuel()
+    heures_semaine = lire_heures_semaine()
+    # ajouter ici tout traitement supplémentaire si nécessaire
+    print(f"Le salaire saisi est de {salaire_mensuel} euros.")
+    print(f"Le nombre d'heures travaillées est de {heures_semaine} heures.")
 
 lire_entrees()
